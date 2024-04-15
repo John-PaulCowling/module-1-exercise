6296a5b5b09f75e9441d04a955a565c8d98a9ddc
John-Paul Cowling <john-paul.cowling@landservices.com.au>
2024-04-15 15:04:44 +0930

    Exec-1

---
+++ exec-1.ts
@@ -0,0 +1,11 @@
+function calculateSquare (value:number):number{
+    return value * value
+}
+
+console.log(calculateSquare(3))
+
+function calculateTriangleArea (base:number, height:number):number{
+    return base * height / 2
+}
+
+console.log(calculateTriangleArea(5,10))
